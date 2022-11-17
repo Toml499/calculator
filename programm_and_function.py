f189da88dd394e508b3e000fc50ff2114b730255
abli_derZweite <fingerboard_fri@yahoo.de>
2022-11-17 22:37:52 +0100

    Auto stash before merge of "main" and "origin/main"

--- programm_and_function.py
+++ programm_and_function.py
@@ -6,13 +6,67 @@ Created on Thu Nov 17 14:00:19 2022
 """
 import time
 
+KMHTOMS = True
+
+def printHelpText():
+    if KMHTOMS:
+        string = '[km/h] -> [m/s]'
+    else:
+        string = '[m/s] -> [km/h]'
+    print('==================================== \n ')
+    print('Die aktuelle Umrechnung ist {}.'.format(string))
+    print('[exit] Beenden Sie das Programm indem sie "exit" eingeben.')
+    print('[/help] Rufen Sie diese Hilfe Funktion auf.')
+    print('[/toggle] Ändern Sie die Umrechnungsrichtung.')
+    print('[/rotate] Für echte Geniesser!')
+    print('[/m/s] Umrechnen in [m/s].')
+    print('[/s/m] Umrechnen in [s/m].')
+    print('\n==================================== \n ')
+    
+def CommandHandler(input_string):
+    global KMHTOMS
+    lwrd = input_string.lower()
+    if lwrd in ['/', '/h', 'help', '/help', '/hilfe', 'hilfe', '/info']:
+        printHelpText()
+        return 2 #exit code 'Change Needed'
+    
+    if lwrd in ['/toggle', '/t']:
+        if KMHTOMS:
+             KMHTOMS = True
+             print('Nun wird von km/h in m/s umgerechnet.')
+        else:
+             KMHTOMS = False
+             print('Nun wird von km/h in s/m umgerechnet.')        
+        return 2 #exit code 'Change Needed'
+    
+    if lwrd in ['/exit', 'exit', 'e', '/e']:
+        print('Vielen Dank für das Nutzen dieses Tools.')
+        print('© Patrick Ablinger, 2022')
+        return 0 #0 is exit key
+    
+    if lwrd in ['/rotate', 'rotate', '/r', 'r']:
+        
+        return 2
+    
+    if lwrd in ['m/s', '/m/s', '[/m/s]', '/[/m/s]']:
+        KMHTOMS = True
+        print('Nun wird von [km/h] in [m/s] umgerechnet.')
+        return 2
+    
+    if lwrd in ['s/m', '/s/m', '[/s/m]', '/[/s/m]']:
+        KMHTOMS = False
+        print('Nun wird von [km/h] in [s/m] umgerechnet.')
+        return 2
+    return 1 #if no keyword --> try continuing processing
+
+
 def loading_function():
     print("Processing Input: ##", end = ' ', flush=True)
     for i in range(6):
-        time.sleep(1)
+        time.sleep(0.2)
         print("##", end=' ', flush=True)
     print(" ")
-    time.sleep(1)
+    time.sleep(0.2)
     print("Calculation Finished!")
     time.sleep(1)
     return
@@ -102,27 +156,49 @@ def validateString(input_string, comma_n):
 
     return out_float, comma_n
         
-   
-#=============================================================================    
+
+def TransTo():
+    meter_sec = (abs(float_valid / 3.6)) #actual calculation into m/s --> abs value because speed is always abs compared to velocity
+    if KMHTOMS:          
+        return meter_sec
+    else:
+        sec_meter = 1/meter_sec #invert
+        return sec_meter
+    
+#=============================================================================
+
+    
+
+print('Falls Sie Hilfe benötigen, geben Sie /help ein.')   
 print('Beenden Sie das Programm indem sie "exit" eingeben.')
 print('Bitte geben Sie die gegebenen km/h ein: ')
-
 while True:  
     kmh = input()
-    if kmh.lower() == 'exit': # .lower to accept any type of written "exit"
-        break #end Programm here
-    
+    continueValue = CommandHandler(kmh)
+    if continueValue == 0:
+        break    #exit main loop
+    if continueValue == 1:
+        pass #do nothing (continue value == 1)
+    if continueValue == 2:
+        print('Entern Sie bitte ihre neue Eingabe.')
+        continue #repeat Process
+
+   
+
     prepared_str, nFloat = prepareString(kmh) #prepare string and number of float values
     float_valid, nFloat = validateString(prepared_str, nFloat) #converting string or restart
 
-    meter_sec = (abs(float_valid / 3.6)) #actual calculation into m/s --> abs value because speed is always abs compared to velocity
-    sec_meter = 1/meter_sec #invert
+    calculated = TransTo()
     
-    #round + 1 because you want to know n+1 comma values
+
     loading_function()
-    print('Ihre Eingabe beträgt umgerechnet {} m/s beziehungsweise {} s/m. \n'.format(round(meter_sec, nFloat+1), round(sec_meter,nFloat+1)))
+    if KMHTOMS:
+        unit = ['[m/s]']
+    else:
+        unit = ['[s/m]']
+    print('Ihre Eingabe beträgt umgerechnet {} {}. \n'.format(round(calculated, nFloat+1), unit[0]))     #round + 1 because you want to know n+1 comma values
     
-print('Vielen Dank für das Nutzen dieses Tools.')
+
 
 
 
